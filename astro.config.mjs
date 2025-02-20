f578f6ad5d50395f7260460b199de8e33c185d73
Gerrett Durbin <gerrettdurbin@gmail.com>
2025-02-20 09:22:17 -0800

    Added docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://rndocs.pages.dev/rgamecore",
+  integrations: [
+    starlight({
+      title: "RADIANTNERD DOCS",
+      social: {
+        github: "https://github.com/gdurbin",
+        patreon: "https://patreon.com/radiantnerd",
+        "x.com": "https://x.com/radiant_nerd",
+        youtube: "https://youtube.com/@radiantnerd",
+        email: "mailto:radiantnrd@gmail.com",
+      },
+      sidebar: [
+        {
+          label: "RGameCore",
+          autogenerate: { directory: "rgamecore" },
+        },
+        {
+          label: "Attribution",
+          items: [{ label: "Attribution", link: "rgamecore/attribution" }],
+        },
+        {
+          label: "Resources",
+          items: [{ label: "GameDev Resources", link: "rgamecore/resources" }],
+        },
+      ],
+      disable404Route: true,
+      components: {
+        SiteTitle: "@components/SiteTitleCustom.astro",
+      },
+    }),
+  ],
 });
